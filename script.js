10ffb0bd8278dd32afa5a655bfe743eed8d8f616
Cross-Rang <Anthonyswaspswarm@gmail.com>
2026-03-05 21:26:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ guestForm.addEventListener('submit', function (e) {
                 <h2>${name}</h2>
                 <p>${comment}</p>;
                 
-
-    guestList.appendChild(guest-card);
+    guestList.appendChild(guestCard);
 
     guestForm.reset();
 });
