99a3f9d8a089f4bf4a5fd812b59d6df17ea04c17
Cross-Rang <Anthonyswaspswarm@gmail.com>
2026-03-05 20:33:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const guestForm = document.getElementById('guestForm');
+const guestList = document.getElementById('guestList');
+
+guestForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const comment = document.getElementById('comment').value;
+
+    const guestCard = document.createElement('div');
+    guestCard.classList.add('guest-card');
+    guestCard.innerHTML = `
+                <h2>${name}</h2>
+                <p>${comment}</p>;
+
+    guestList.appendChild(guestCard);
+
+    guestForm.reset();
+});
