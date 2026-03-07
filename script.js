037a2e5d370e1591703e60745c2532cb0cbc4b45
Cross-Rang <Anthonyswaspswarm@gmail.com>
2026-03-06 21:49:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ guestForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const name = document.getElementById('name').value;
-    const address = document.getElementById('address').value;
+    const address = document.getElementById('site').value;
+    const mobile = document.getElementById('rating').value;
+    const roomno = document.getElementById('comment').value;
 
     const guestCard = document.createElement('div');
     guestCard.classList.add('guest-card');
     guestCard.innerHTML = `
-                <h2>${name}</h2>
-                <p><strong>Comment:</strong> ${address}</p>
-                
+                <h2>I, ${name}, went to Hell and all I got was this card.</h2>
+                <p><strong>Where I found Hell:</strong> ${site}</p>
+                <p><strong>Opinion of Hell:</strong> ${rating}</p>
+                <p><strong>Additional Comments:</strong> ${comment}</p>
+                <p><strong>We hope to see you again sometime!</strong></p>`;
 
     guestList.appendChild(guestCard);
 
