b0b6be95872ba834ec255eb0d2078568a4968e11
Cross-Rang <Anthonyswaspswarm@gmail.com>
2026-03-06 21:10:55 -0500

    Script test

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ guestForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const name = document.getElementById('name').value;
-    const comment = document.getElementById('comment').value;
+    const address = document.getElementById('address').value;
+    const mobile = document.getElementById('mobile').value;
+    const roomno = document.getElementById('roomno').value;
 
-    const ul = document.getElementById("guestList");
-    const guestCard = document.createElement('li');
-    guestCard.innerHTML =
-                <p><strong>${name}:<strong>${comment}</p>;
-                
-    guestList.append(guestCard);
+    const guestCard = document.createElement('div');
+    guestCard.classList.add('guest-card');
+    guestCard.innerHTML = `
+                <h2>${name}</h2>
+                <p><strong>Address:</strong> ${address}</p>
+                <p><strong>Mobile:</strong> ${mobile}</p>
+                <p><strong>Room Number:</strong> ${roomno}</p>`;
+
+    guestList.appendChild(guestCard);
 
     guestForm.reset();
 });
