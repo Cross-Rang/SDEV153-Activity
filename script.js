5c4004861208c374fd66905cf67b98c469f0c463
Cross-Rang <Anthonyswaspswarm@gmail.com>
2026-03-06 22:03:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ guestForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const name = document.getElementById('name').value;
-    const address = document.getElementById('site').value;
-    const mobile = document.getElementById('rating').value;
-    const roomno = document.getElementById('comment').value;
+    const site = document.getElementById('site').value;
+    const rating = document.getElementById('rating').value;
+    const comment = document.getElementById('comment').value;
 
     const guestCard = document.createElement('div');
     guestCard.classList.add('guest-card');
     guestCard.innerHTML = `
-                <h2>I, ${name} , went to Hell and all I got was this card.</h2>
+                <h2>I, ${name}, went to Hell and all I got was this card.</h2>
                 <p><strong>Where I found Hell:</strong> ${site} </p>
                 <p><strong>Opinion of Hell:</strong> ${rating} </p>
                 <p><strong>Additional Comments:</strong> ${comment} </p>
